ebe14b1386c0b8cd49edb1c2936cef583c731979
Jet-Etnyre <jetetnyre@gmail.com>
2025-12-06 02:35:02 -0600

    intial setup for client

--- InkRush/src/CanvasController.java
+++ InkRush/src/CanvasController.java
@@ -3,14 +3,27 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
 
 public class CanvasController {
+
+    @FXML
+    private Button chatButton;
+
+    @FXML
+    private TextArea chatTextArea;
+
+    @FXML
+    private TextField chatTextInput;
 
     @FXML
     private Button clearButton;
 
     @FXML
     private Canvas drawingCanvas;
+
+    @FXML
+    private Button nameButton;
 
     @FXML
     private TextField playerNameField;
