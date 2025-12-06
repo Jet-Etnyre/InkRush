c961db25866cf3fdf8591732943745880f237037
elifoztu <eozturk@uiowa.edu>
2025-12-06 10:35:27 -0600

    Message class using Serializable started

---
+++ InkRush/src/Message.java
@@ -0,0 +1,12 @@
+import java.io.Serializable;
+
+/**
+ * This class represents a message in the game, the messages get passed between client and server with
+ * different types.
+ *
+ * Supported message types include CONNECTED, CHAT, DRAW,
+ */
+
+public class Message implements Serializable {
+
+}
