e21314d56d8e132a0c32065b704070f3069d5538
elifoztu <eozturk@uiowa.edu>
2025-12-05 20:13:43 -0600

    Client and ClientDriver base code changed from Swing to JavaFX.

--- InkRush/src/ClientDriver.java
+++ InkRush/src/ClientDriver.java
@@ -1,2 +1,8 @@
+// JavaFX version of ClientDriver.java
+
 public class ClientDriver {
+    public static void main(String[] args) {
+        // Just delegate to the JavaFX Client main, which calls Application.launch
+        Client.main(args);
+    }
 }
