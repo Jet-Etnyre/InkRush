ab3c9321cd670499189a6f48458af029f9fa01ba
elifoztu <eozturk@uiowa.edu>
2025-12-05 21:02:34 -0600

    Client and ClientDriver classes deleted

--- InkRush/src/ClientDriver.java
+++
@@ -1,8 +0,0 @@
-// JavaFX version of ClientDriver.java
-
-public class ClientDriver {
-    public static void main(String[] args) {
-        // Just delegate to the JavaFX Client main, which calls Application.launch
-        Client.main(args);
-    }
-}
