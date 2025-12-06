2c1a73355639e50d6cc064b40c49b76ecda067ce
Nomi Wang <zhanghe-wang@uiowa.edu>
2025-12-06 13:14:54 -0600

    ENH:Implemented logic to pass username from Lobby to CanvasController

--- InkRush/src/LobbyController.java
+++ InkRush/src/LobbyController.java
@@ -1,6 +1,14 @@
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+import java.io.IOException;
 
 public class LobbyController {
 
@@ -10,4 +18,38 @@ public class LobbyController {
     @FXML
     private TextField playerNameField;
 
+    @FXML
+    public void onJoinClicked() {
+        try {
+            String username = playerNameField.getText().trim();
+
+            if (username.isEmpty()) {
+                System.out.println("Please enter a name!");
+                return;
+            }
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Canvas.fxml"));
+            Parent gameRoot = loader.load();
+
+            // Get the controller and set the data
+            final CanvasController gameController = loader.getController();
+            gameController.setPlayerName(username);
+
+            Stage stage = (Stage) joinButton.getScene().getWindow();
+
+            // ANONYMOUS CLASS (No Lambda)
+            stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+                @Override
+                public void handle(WindowEvent event) {
+                    gameController.disconnect();
+                }
+            });
+
+            stage.setScene(new Scene(gameRoot));
+            stage.setTitle("InkRush - Game: " + username);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
