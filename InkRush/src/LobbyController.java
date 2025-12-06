a95f83165b284c0e228addac13becd8e9011dcb3
Amit Boodhoo <amit.boodhoo@example.com>
2025-12-05 22:02:10 -0600

    Added the skeleton LobbyController with the FXML fields

--- InkRush/src/LobbyController.java
+++ InkRush/src/LobbyController.java
@@ -1,2 +1,13 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class LobbyController {
+
+    @FXML
+    private Button joinButton;
+
+    @FXML
+    private TextField playerNameField;
+
 }
